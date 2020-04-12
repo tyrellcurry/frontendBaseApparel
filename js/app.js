f516357a8c02fd693b9a599326a249f8f39d58e3
Tyrell Curry <tyrellrcurry@gmail.com>
2020-04-11 22:29:28 -0700

    Committing finished design

---
+++ js/app.js
@@ -0,0 +1,11 @@
+const myForm = document.getElementById("form");
+
+myForm.addEventListener("submit", (e) =>{
+	e.preventDefault();
+
+	console.log("Form has been submitted.")
+});
+ function btnSubmit(){
+ 	console.log("hey")
+ };
+
