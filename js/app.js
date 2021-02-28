06ad0f8f89dfe31470949452f6dcd79d6e4dc22a
Tyrell Curry <tyrellrcurry@gmail.com>
2021-02-28 00:37:38 -0800

    Big update and general fix to all elements

--- js/app.js
+++ js/app.js
@@ -1,11 +1,17 @@
-const myForm = document.getElementById("form");
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+function onSubmit() {
+    var emailInput = document.getElementById("emailInput");
+    var errorText = document.getElementById("errorText");
+    var emailAddress = document.getElementById("emailAddress").value;
 
-myForm.addEventListener("submit", (e) =>{
-	e.preventDefault();
-
-	console.log("Form has been submitted.")
-});
- function btnSubmit(){
- 	console.log("hey")
- };
-
+    if (validateEmail(emailAddress)) {
+        emailInput.classList.remove("incorrect");
+        errorText.classList.add("invisible");
+    } else {
+        emailInput.classList.add("incorrect");
+        errorText.classList.remove("invisible");
+    }
+}
